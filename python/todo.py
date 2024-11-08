088f95bd445584e8baf22cfad433b73e42c665dc
Jon Hill <jon.hill@york.ac.uk>
2024-11-08 13:13:36 +0000

    Adding doc string

--- python/todo.py
+++ python/todo.py
@@ -1,13 +1,30 @@
+"""
+todo.py is a simple to-do list application that
+keeps a users to-do list as a text file.
+
+Users can add, list or remove tasks
+"""
 import argparse
 import os
 
 TASK_FILE = ".tasks.txt"
 
 def add_task(task):
+    """Function: add_task
+    
+    Input - a task to add to the list
+    Return - nothing
+    """
     with open(TASK_FILE, "a", encoding="utf-8") as file:
         file.write(task + "\n")
 
 def list_tasks():
+    """Function: list_tasks
+
+    Input - a task to add to the list
+    Return - a string of the tasks on the list
+    """
+
     output_string = ""
     if os.path.exists(TASK_FILE):
         with open(TASK_FILE, "r", encoding="utf-8") as file:
@@ -15,10 +32,16 @@ def list_tasks():
             for index, task in enumerate(tasks, start=1):
                 output_string += (f"{index}. {task.strip()}\n")
     return output_string.strip()
-        
+
 
 
 def remove_task(index):
+    """Function: remove_task
+
+    Input - a task to add to the list
+    Return - nothing
+    """
+
     if os.path.exists(TASK_FILE):
         with open(TASK_FILE, "r", encoding="utf-8") as file:
             tasks = file.readlines()
@@ -32,6 +55,12 @@ def remove_task(index):
 
 
 def main():
+    """Function: main
+    
+    Input - nothing
+    Return - nothing
+    """
+
     parser = argparse.ArgumentParser(description="Command-line Todo List")
     parser.add_argument(
             "-a",
