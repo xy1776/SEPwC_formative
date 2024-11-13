3133604b490ebe98aceba51f5f1b7a7526e38f78
Jon Hill <jon.hill@york.ac.uk>
2024-11-13 16:06:52 +0000

    almost done with testing

--- R/test/test_script.R
+++ R/test/test_script.R
@@ -13,47 +13,112 @@ library(testthat)
 #
 
 # load in the script you want to test
-source("../todo.R")
-test_data <- "test_list.txt"
+source("../todo.R", local=TRUE)
+test_file <- "test_list.txt"
 
 
 # tests --------------------
-# check the get_plot_limit function
-  test_that("list_tasks", {
-    TASK_FILE <- ""
-  })
-
-  test_that("remove_task", {
-    
-    
-  })
-
-  test_that("add_task", {
-
-    
-  })
-
-  test_that("check main", {
-      args <- c("list"=TRUE)
-      args <- data.frame(t(args))
-      main(args)
-  })
-
-
-  if (requireNamespace("lintr")) {
-    library(lintr)
+test_that("list_tasks works correctly", {
+  # Assuming test_file is defined and the necessary setup is done
   
-    context("linting script")
-    test_that("Coding style", {
-      output<-lintr::lint("../todo.R")
-      expect_lt(length(output),500)
-      expect_lt(length(output),400)
-      expect_lt(length(output),250)
-      expect_lt(length(output),100)
-      expect_lt(length(output),50)
-      expect_lt(length(output),10)
-      expect_equal(length(output),0)
-    })
-  }
+  TASK_FILE <<- test_file
+  
+  task_list <- list_tasks()
+  expected_list <- "1. Item 1\n2. Item 2\n3. Item 3\n4. Item 4\n5. Item 5"
+  
+  expect_equal(task_list, expected_list)
+})
+
+test_that("remove_task works correctly", {
+  # Assuming test_file is defined and the necessary setup is done
+  
+  temp_list <- ".temp_test.txt"
+  file.copy(test_file, temp_list)
+  TASK_FILE <<- temp_list
+  
+  # Test invalid index (should error)
+  expect_error(remove_task(0))
+  
+  # Test invalid index (should error)
+  expect_error(remove_task(10))
+  
+  # Remove the first task
+  remove_task(1)
+  
+  task_list <- list_tasks()
+  new_expected <- "1. Item 2\n2. Item 3\n3. Item 4\n4. Item 5"
+  
+  expect_equal(task_list, new_expected)
+  
+  file.remove(temp_list)
+})
+
+test_that("add_task works correctly", {
+  # Assuming test_file is defined and the necessary setup is done
+
+  temp_list <- ".temp_test.txt"
+  file.copy(test_file, temp_list)
+  TASK_FILE <<- temp_list
+
+  add_task("Item 6")
+
+  task_list <- list_tasks()
+  expected_list <- "1. Item 1\n2. Item 2\n3. Item 3\n4. Item 4\n5. Item 5\n6. Item 6"
+
+  expect_equal(task_list, expected_list)
+
+  file.remove(temp_list)
+})
+
+test_that("check main", {
+
+  temp_list <- ".temp_test.txt"
+  file.copy(test_file, temp_list)
+  TASK_FILE <<- temp_list
+
+  args <- c("list" = TRUE)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_equal(nchar(output), 59)
+
+  args <- c("add" = "Item 6")
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  print(output)
+  expect_identical(output, character(0))
+
+  args <- c("list" = TRUE)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_equal(nchar(output), 70)
+  expected_list <- "[1] \"1. Item 1\\n2. Item 2\\n3. Item 3\\n4. Item 4\\n5. Item 5\\n6. Item 6\""
+  expect_equal(output, expected_list)
+
+  args <- c("remove" = 6)
+  args <- data.frame(t(args))
+  output <- capture.output(main(args))
+  expect_equal(nchar(output), 59)
+
+  
+  file.remove(temp_list)
+
+})
+
+
+if (requireNamespace("lintr")) {
+library(lintr)
+
+context("linting script")
+test_that("Coding style", {
+  output<-lintr::lint("../todo.R")
+  expect_lt(length(output),500)
+  expect_lt(length(output),400)
+  expect_lt(length(output),250)
+  expect_lt(length(output),100)
+  expect_lt(length(output),50)
+  expect_lt(length(output),10)
+  expect_equal(length(output),0)
+})
+}
 
 
--- R/todo.R
+++ R/todo.R
@@ -1,57 +1,70 @@
 #!/usr/bin/env Rscript
 suppressPackageStartupMessages({
-library(argparse)
+  library(argparse)
 })
 
-TASK_FILE <- ".tasks.txt"
+TASK_FILE <- ".tasks.txt" # nolint
 
 add_task <- function(task) {
-
-
+  write(task, file = TASK_FILE, append = TRUE, sep = "\n")
 }
 
-list_tasks <- function(task) {
-
-
+list_tasks <- function() {
+  if (file.exists(TASK_FILE)) {
+    tasks <- readLines(TASK_FILE)
+    output_string <- paste(seq_along(tasks), tasks, sep = ". ", collapse = "\n")
+    return(output_string)
+  } else {
+    return("No tasks found.")
+  }
 }
 
 remove_task <- function(index) {
 
-
+  tasks <- NA
+  if (file.exists(TASK_FILE)) {
+    tasks <- readLines(TASK_FILE)
+  } else {
+    stop("File not found")
+  }
+  if (index <= length(tasks)) {
+    tasks <- tasks[-index]
+    if (identical(tasks, character(0))) {
+      stop("Task not found")
+    }
+    writeLines(tasks, TASK_FILE)
+    print("Task removed.")
+  } else {
+    stop("No tasks found.")
+  }
 }
 
 main <- function(args) {
-
-    print(args)
-        
-    if (args$add) {
-        add_task(args$add)
-    } else if (args$list) {
-        tasks <- list_tasks()
-        print(tasks)
-    } else if (args$remove) {
-        remove_task(args$remove)
-    } else {
-        print("Use --help to get help on using this program")
-    }
-    
+  if (!is.null(args$add)) {
+    add_task(args$add)
+  } else if (args$list) {
+    tasks <- list_tasks()
+    print(tasks)
+  } else if (!is.null(args$remove)) {
+    remove_task(args$remove)
+  } else {
+    print("Use --help to get help on using this program")
+  }
 }
 
 
-if(sys.nframe() == 0) {
+if (sys.nframe() == 0) {
 
   # main program, called via Rscript
-  parser = ArgumentParser(
-                    description="Command-line Todo List"
-                    )
-  parser$add_argument('-a', '--add',
-                    help="Add a new task")
-  parser$add_argument('-l', '--list',
-                    action='store_true',
-                    help="List all tasks")
-  parser$add_argument('-r', '--remove',
-                    help="Remove a task by index")
+  parser <- ArgumentParser(description = "Command-line Todo List")
+  parser$add_argument("-a", "--add",
+                      help = "Add a new task")
+  parser$add_argument("-l", "--list",
+                      action = "store_true",
+                      help = "List all tasks")
+  parser$add_argument("-r", "--remove",
+                      help = "Remove a task by index")
 
-  args = parser$parse_args()  
+  args <- parser$parse_args()
   main(args)
 }
